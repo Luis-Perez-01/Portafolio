448d16d0744cbc9805e2043f8cac65a6e768b1ba
Luis-Perez-01 <LuisM1P3@hotmail.com>
2023-08-05 11:14:42 -0600

    Adding To Do App

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -7,7 +7,9 @@ export default function Projects() {
 
   return (
     <>
-      <h1 className="max-w-screen-xl lg:mx-auto m-10 text-2xl font-bold my-10">Mis proyectos</h1>
+      <h1 className="max-w-screen-xl lg:mx-auto m-10 text-2xl font-bold my-10">
+        Mis proyectos
+      </h1>
       <section className="max-w-screen-xl lg:mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center bg-gray-100 dark:bg-slate-900/50 rounded-lg p-6 m-10 gap-20 animate-fade-down">
         <div className="max-w-[19.13rem] mx-auto">
           <div className="overflow-hidden rounded-t-lg">
@@ -67,7 +69,10 @@ export default function Projects() {
         </div>
         <div className="max-w-[19.13rem] mx-auto">
           <div className="overflow-hidden  rounded-t-lg">
-            <a href="https://tic-tac-toe-kappa-coral.vercel.app" target="_blank">
+            <a
+              href="https://tic-tac-toe-kappa-coral.vercel.app"
+              target="_blank"
+            >
               <img
                 className="max-w-[19.13rem] max-h-[38.5rem] object-cover hover:scale-105 duration-300"
                 src="/assets/tic-tac-toe.png"
@@ -91,6 +96,20 @@ export default function Projects() {
           </div>
           <div className="text-center font-semibold bg-gray-200 dark:bg-gray-800 rounded-b-lg px-3 py-2">
             Newsletter
+          </div>
+        </div>
+        <div className="max-w-[19.13rem] mx-auto">
+          <div className="overflow-hidden  rounded-t-lg">
+            <a href="https://to-do-ecru-seven.vercel.app/" target="_blank">
+              <img
+                className="max-w-[18rem] h-[38.5rem] object-cover hover:scale-105 duration-300"
+                src="/assets/ToDo.png"
+                alt="To Do App"
+              />
+            </a>
+          </div>
+          <div className="text-center font-semibold bg-gray-200 dark:bg-gray-800 rounded-b-lg px-3 py-2">
+            To Do App
           </div>
         </div>
       </section>
