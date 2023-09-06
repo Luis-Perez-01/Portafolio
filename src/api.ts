8dc9709a47586722cd3ec424aa81a3fce6c97720
Luis-Perez-01 <LuisM1P3@hotmail.com>
2023-09-05 21:13:15 -0600

    Cambiando URL API

--- src/api.ts
+++ src/api.ts
@@ -4,27 +4,33 @@ const api = {
   auth: {
     login: {
       fetch: async (email: string, password: string) => {
-        const response = await fetch("http://localhost:3000/api/auth/signin", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-          body: JSON.stringify({ email, password }),
-        }).then((res) => res.json());
+        const response = await fetch(
+          "https://portafolio-backend-ihwm-dev.fl0.io/api/auth/signin",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify({ email, password }),
+          }
+        ).then((res) => res.json());
         return response;
       },
     },
     register: {
       fetch: async (name: string, email: string, password: string) => {
-        const response = await fetch("http://localhost:3000/api/auth/signup", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-          body: JSON.stringify({ name, email, password }),
-        }).then((res) => res.json());
+        const response = await fetch(
+          "https://portafolio-backend-ihwm-dev.fl0.io/api/auth/signup",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify({ name, email, password }),
+          }
+        ).then((res) => res.json());
         return response;
       },
     },
@@ -32,29 +38,38 @@ const api = {
   posts: {
     getAll: {
       fetch: async () => {
-        const response = await fetch("http://localhost:3000/api/posts", {
-          method: "GET",
-          credentials: "include",
-        }).then((res) => res.json() as Promise<Post[]>);
+        const response = await fetch(
+          "https://portafolio-backend-ihwm-dev.fl0.io/api/posts",
+          {
+            method: "GET",
+            credentials: "include",
+          }
+        ).then((res) => res.json() as Promise<Post[]>);
         return response;
       },
     },
     getOne: {
       fetch: async (id: string | undefined) => {
-        const response = await fetch(`http://localhost:3000/api/posts/${id}`, {
-          method: "GET",
-          credentials: "include",
-        }).then((res) => res.json());
+        const response = await fetch(
+          `https://portafolio-backend-ihwm-dev.fl0.io/api/posts/${id}`,
+          {
+            method: "GET",
+            credentials: "include",
+          }
+        ).then((res) => res.json());
         return response;
       },
     },
     create: {
       fetch: async (FormData: any) => {
-        const response = await fetch("http://localhost:3000/api/posts", {
-          method: "POST",
-          credentials: "include",
-          body: FormData,
-        }).then((res) => res.json());
+        const response = await fetch(
+          "https://portafolio-backend-ihwm-dev.fl0.io/api/posts",
+          {
+            method: "POST",
+            credentials: "include",
+            body: FormData,
+          }
+        ).then((res) => res.json());
         return response;
       },
     },
