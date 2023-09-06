cf55ca1d1550554f12a51cdc464d3ac5adb205b8
Luis-Perez-01 <LuisM1P3@hotmail.com>
2023-09-06 11:22:38 -0600

    Styling header blog

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -12,7 +12,7 @@ export default function Blog() {
 
   return (
     <div className="max-w-screen-lg my-10 mx-auto animate-fade-down">
-      <div className="flex justify-between items-center">
+      <div className="max-w-screen-lg flex justify-between items-center mb-10">
         <h1 className="font-semibold text-2xl">Últimos artículos</h1>
         <Link
           to="/blog/create"
