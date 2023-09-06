5127566ada698e35330dbc32f682ffb5ecbf12df
Luis-Perez-01 <LuisM1P3@hotmail.com>
2023-09-06 10:51:53 -0600

    Adding button to create blog

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import api from "../../api";
 import PostList from "../../components/PostList";
+import { Link } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
 
 export default function Blog() {
   const [posts, setPosts] = useState<any>([]);
+  const { id } = useContext(UserContext);
 
   useEffect(() => {
     api.posts.getAll.fetch().then((data) => setPosts(data));
@@ -11,7 +14,19 @@ export default function Blog() {
 
   return (
     <div className="max-w-screen-lg my-10 mx-auto animate-fade-down">
-      <h1 className="font-semibold text-2xl mb-10 mx-10">Últimos artículos</h1>
+      <div className="flex justify-between items-center">
+        <h1 className="font-semibold text-2xl">Últimos artículos</h1>
+        {id ? (
+          <Link
+            to="/blog/create"
+            className="rounded-lg text-white text-sm font-semibold bg-blue-700 hover:bg-blue-600 hover:scale-105 tranform ease-in-out duration-75 px-3 py-2 drop-shadow"
+          >
+            Crear artículo
+          </Link>
+        ) : (
+          ""
+        )}
+      </div>
       <PostList posts={posts} />
     </div>
   );
