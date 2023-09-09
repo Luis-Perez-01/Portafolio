57a39b60afe7020b8e17c8d704a6a4883f7b79cf
Luis-Perez-01 <LuisM1P3@hotmail.com>
2023-09-08 20:21:40 -0600

    vercel config

--- src/pages/blog/Post.tsx
+++ src/pages/blog/Post.tsx
@@ -30,7 +30,7 @@ export default function Post() {
         </Link>
         <span>{formatDate(post.createdAt)}</span>
       </div>
-      <article>
+      <article className="max-w-screen-lg mx-10 md:mx-auto">
         <section className="space-y-8">
           <h1 className="text-4xl font-semibold">{post.title}</h1>
           <p className="text-2xl">{post.description}</p>
