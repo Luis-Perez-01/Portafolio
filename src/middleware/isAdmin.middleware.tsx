63e3e9bb5b4fd60b6128a52c5dffbe3099578be4
Luis-Perez-01 <LuisM1P3@hotmail.com>
2023-09-15 20:07:47 -0600

    Adding IsAdmin Middleware

--- src/middleware/isAdmin.middleware.tsx
+++ src/middleware/isAdmin.middleware.tsx
@@ -3,7 +3,6 @@ import { UserContext } from "../context/UserContext";
 
 export default function IsAdmin({ children }: any) {
   const { userInfo } = useContext(UserContext);
-  console.log(userInfo);
 
   if (userInfo !== null) {
     if (userInfo.id === "64f7f14111e13ad18a78c3a3") {
