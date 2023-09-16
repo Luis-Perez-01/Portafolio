77a05130706305da5a633eaa95c6d37d4267408d
Luis-Perez-01 <LuisM1P3@hotmail.com>
2023-09-15 20:07:31 -0600

    Adding IsAdmin Middleware

--- src/middleware/isAdmin.middleware.tsx
+++ src/middleware/isAdmin.middleware.tsx
@@ -3,25 +3,21 @@ import { UserContext } from "../context/UserContext";
 
 export default function IsAdmin({ children }: any) {
   const { userInfo } = useContext(UserContext);
+  console.log(userInfo);
 
   if (userInfo !== null) {
     if (userInfo.id === "64f7f14111e13ad18a78c3a3") {
       return <>{children}</>;
-    } else {
-      return (
-        <>
-          <div className="max-w-screen-lg w-full my-10 mx-auto animate-fade-down">
-            <div className="max-w-screen-lg flex justify-between items-center mx-10 mb-10">
-              <h1 className="font-semibold text-2xl">Últimos artículos</h1>
-            </div>
-            <div className="flex justify-center items-center">
-              <h1 className="font-semibold text-2xl">
-                No tienes permisos para ver esta página
-              </h1>
-            </div>
-          </div>
-        </>
-      );
     }
+  } else {
+    return (
+      <>
+        <div className="max-w-screen-lg w-full my-10 mx-auto animate-fade-down">
+          <div className="max-w-screen-lg flex justify-between items-center mx-10 mb-10">
+            <h1 className="font-semibold text-2xl">Últimos artículos</h1>
+          </div>
+        </div>
+      </>
+    );
   }
 }
