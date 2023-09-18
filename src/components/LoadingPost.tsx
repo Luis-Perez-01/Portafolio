3aafef8d7930091bca835647de0a67e868c10e26
Luis-Perez-01 <LuisM1P3@hotmail.com>
2023-09-17 23:47:22 -0600

    adding responsive design

--- src/components/LoadingPost.tsx
+++ src/components/LoadingPost.tsx
@@ -1,12 +1,12 @@
 export default function LoadingPost() {
   return (
-    <div className="max-w-screen-lg min-w-screen-lg w-full my-10 px-5 lg:px-0 mx-auto">
+    <div className="max-w-screen-lg min-w-screen-lg w-full my-10 px-2 lg:px-0 mx-auto">
       <div className="animate-pulse">
-        <div className="flex justify-between mb-10 mx-10 md:mx-auto">
+        <div className="flex justify-between mb-10 md:mx-auto">
           <div className="w-16 h-4 bg-gray-300 dark:bg-gray-700"></div>
           <div className="w-36 h-4 bg-gray-300 dark:bg-gray-700"></div>
         </div>
-        <div className="my-10 mx-10 md:mx-auto space-y-1">
+        <div className="my-10 md:mx-auto space-y-1">
           <div className="w-full h-7 bg-gray-300 dark:bg-gray-700"></div>
           <div className="w-3/4 h-7 bg-gray-300 dark:bg-gray-700"></div>
           <div className="space-y-2 pt-8">
--- src/pages/blog/Post.tsx
+++ src/pages/blog/Post.tsx
@@ -24,20 +24,20 @@ export default function Post() {
   return isLoading ? (
     <LoadingPost />
   ) : (
-    <div className="my-10 mx-5 lg:mx-auto">
-      <div className="max-w-screen-lg flex justify-between mb-10 mx-10 md:mx-auto">
+    <div className="my-10 mx-2 lg:mx-auto text-sm md:text-base">
+      <div className="max-w-screen-lg flex justify-between mb-10 md:mx-auto">
         <Link className="flex items-center" to={`/blog`}>
           <ChevronLeft /> Volver
         </Link>
         <span>{formatDate(post.createdAt)}</span>
       </div>
-      <article className="max-w-screen-lg prose prose-2xl dark:prose-invert mx-10 md:mx-auto">
+      <article className="max-w-screen-lg prose prose-2xl dark:prose-invert md:mx-auto">
         <section className="space-y-8">
-          <h1 className="text-4xl font-semibold">{post.title}</h1>
-          <p className="text-2xl">{post.description}</p>
+          <h1 className="text-xl md:text-4xl font-semibold">{post.title}</h1>
+          <p className="text-lg md:text-2xl">{post.description}</p>
           <img src={post.file} alt={post.title} className="mx-auto" />
           <div
-            className="text-lg"
+            className="text-base md:text-lg"
             dangerouslySetInnerHTML={{ __html: post.content }}
           />
         </section>
