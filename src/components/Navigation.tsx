66c0f9211917252b386724b7205760bcbd683cf5
Luis-Perez-01 <LuisM1P3@hotmail.com>
2023-10-10 17:03:39 -0600

    Adding new navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import { BadgeCheck, Menu, Moon, Sun } from "lucide-react";
+import { BadgeCheck, Moon, Sun } from "lucide-react";
 import { useContext, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import ThemeContext from "../context/ThemeContext";
@@ -6,15 +6,25 @@ import useMenu from "../hooks/ToggleMenuHook";
 import { ModalContext } from "../context/ModalContext";
 import { UserContext } from "../context/UserContext";
 import jwt from "jwt-decode";
+import {
+  Button,
+  Link,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenu,
+  NavbarMenuToggle,
+} from "@nextui-org/react";
 
 export default function Navigation() {
-  const { theme, toggleTheme } = useContext<any>(ThemeContext);
-
   const { handleModalLogin } = useContext<any>(ModalContext);
 
   const { userInfo, setUserInfo } = useContext(UserContext);
 
-  const { toogleMenu, handleClick } = useMenu();
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  const { handleClick } = useMenu();
 
   const navigate = useNavigate();
 
@@ -45,123 +55,161 @@ export default function Navigation() {
   }
 
   return (
-    <header className="dark:border-b dark:border-gray-800 drop-shadow-sm dark:text-white sticky bg-white/70 dark:bg-gray-900/90 backdrop-blur-md top-0 z-10">
-      <nav className="flex flex-wrap justify-between items-center max-w-screen-lg mx-auto p-4">
-        <NavLink
-          onClick={() => {
-            window.scrollTo(0, 0);
-          }}
-          className="flex items-center "
-          to="/"
-        >
-          <img src="/assets/me.jpg" className="w-10 h-10 rounded-full mr-2" />
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white hover:text-blue-700 dark:hover:text-blue-700">
-            Luis Pérez
-          </span>
-          <BadgeCheck className="text-blue-700 ml-1" />
-        </NavLink>
-
-        <button
-          onClick={handleClick}
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        >
-          <Menu />
-        </button>
-        <div
-          className={`w-full border border-gray-100 rounded-lg md:mt-0 md:border-0 dark:border-gray-700 my-4 md:my-0 md:block md:w-auto ${
-            toogleMenu === false ? "hidden" : ""
-          }`}
-        >
-          <ul className="w-fit m-auto font-medium items-center flex flex-col p-4 md:p-0 md:flex-row md:space-x-8 space-y-2 md:space-y-0">
-            <li>
-              <a
-                className="block py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-blue-500 dark:hover:bg-transparent"
-                href="#projects"
-                onClick={() => {
-                  handleClick();
-                  navigate("/");
-                }}
-              >
-                Proyectos
-              </a>
-            </li>
-
-            <a
-              className="block py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-blue-500 dark:hover:bg-transparent"
-              href="#aboutMe"
-              onClick={() => {
-                handleClick();
-                navigate("/");
-              }}
-            >
-              Acerca de mí
-            </a>
-
-            <li>
-              <NavLink
-                className={({ isActive, isPending }) =>
-                  isPending
-                    ? "block py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                    : isActive
-                    ? "border-b-2 border-blue-700"
-                    : ""
-                }
-                onClick={() => {
-                  window.scrollTo(0, 0);
-                  handleClick();
-                }}
-                to="blog"
-              >
-                Blog
-              </NavLink>
-            </li>
-
-            {userInfo ? (
-              <li>
-                <button
-                  onClick={destroySession}
-                  className="rounded-lg text-white text-sm font-semibold bg-gray-700 hover:bg-gray-600 hover:scale-105 tranform ease-in-out duration-75 px-3 py-2 drop-shadow"
-                >
-                  Cerrar
-                </button>
-              </li>
+    <Navbar className="dark:bg-gray-900/90" onMenuOpenChange={handleClick}>
+      <NavbarContent>
+        <NavbarBrand>
+          <NavLink
+            onClick={() => {
+              window.scrollTo(0, 0);
+            }}
+            className="flex items-center "
+            to="/"
+          >
+            <img src="/assets/me.jpg" className="w-10 h-10 rounded-full mr-2" />
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white hover:text-blue-700 dark:hover:text-blue-700">
+              Luis Pérez
+            </span>
+            <BadgeCheck className="text-blue-700 ml-1" />
+          </NavLink>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent className="hidden sm:flex gap-4" justify="end">
+        <NavbarItem>
+          <Link
+            className="block py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-blue-500 dark:hover:bg-transparent"
+            href="#projects"
+            onClick={() => {
+              handleClick();
+              navigate("/");
+            }}
+          >
+            Proyectos
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            className="block py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-blue-500 dark:hover:bg-transparent"
+            href="#aboutMe"
+            onClick={() => {
+              handleClick();
+              navigate("/");
+            }}
+          >
+            Acerca de mí
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <NavLink
+            className="block py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-blue-500 dark:hover:bg-transparent"
+            to="blog"
+            onClick={() => {
+              handleClick();
+              navigate("/");
+            }}
+          >
+            Blog
+          </NavLink>
+        </NavbarItem>
+        {userInfo ? (
+          <Button onClick={destroySession}>Cerrar</Button>
+        ) : (
+          <Button color="primary" onClick={handleModalLogin}>
+            Iniciar
+          </Button>
+        )}
+        <label className="relative inline-flex float-right items-center cursor-pointer">
+          <input
+            type="checkbox"
+            onChange={toggleTheme}
+            className="sr-only peer"
+            checked={theme}
+          />
+          <div className="place-items-center w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+            {theme ? (
+              <Moon
+                size={16}
+                className="h-full mx-1 animate-rotate-y animate-once animate-duration-300"
+              />
             ) : (
-              <li>
-                <button
-                  onClick={handleModalLogin}
-                  className="rounded-lg text-white text-sm font-semibold bg-blue-700 hover:bg-blue-600 hover:scale-105 tranform ease-in-out duration-75 px-3 py-2 drop-shadow"
-                >
-                  Iniciar
-                </button>
-              </li>
+              <Sun
+                size={16}
+                className="h-full mx-1 float-right animate-rotate-y animate-once animate-duration-300"
+              />
             )}
-
-            <li>
-              <label className="relative inline-flex float-right items-center cursor-pointer">
-                <input
-                  type="checkbox"
-                  onChange={toggleTheme}
-                  className="sr-only peer"
-                  checked={theme}
+          </div>
+        </label>
+      </NavbarContent>
+      <NavbarContent className="sm:hidden" justify="end">
+        {userInfo ? (
+          <Button onClick={destroySession}>Cerrar</Button>
+        ) : (
+          <Button color="primary" onClick={handleModalLogin}>
+            Iniciar
+          </Button>
+        )}
+        <NavbarMenuToggle className="sm:hidden" />
+      </NavbarContent>
+      <NavbarMenu>
+        <NavbarItem>
+          <Link
+            className="block py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-blue-500 dark:hover:bg-transparent"
+            href="#projects"
+            onClick={() => {
+              handleClick();
+              navigate("/");
+            }}
+          >
+            Proyectos
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link
+            className="block py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-blue-500 dark:hover:bg-transparent"
+            href="#aboutMe"
+            onClick={() => {
+              handleClick();
+              navigate("/");
+            }}
+          >
+            Acerca de mí
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <NavLink
+            className="block py-2 pl-3 pr-4 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white dark:hover:text-blue-500 dark:hover:bg-transparent"
+            to="blog"
+            onClick={() => {
+              handleClick();
+              navigate("/");
+            }}
+          >
+            Blog
+          </NavLink>
+        </NavbarItem>
+        <NavbarItem>
+          <label className="relative float-right items-center cursor-pointer">
+            <input
+              type="checkbox"
+              onChange={toggleTheme}
+              className="sr-only peer"
+              checked={theme}
+            />
+            <div className="place-items-center w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
+              {theme ? (
+                <Moon
+                  size={16}
+                  className="h-full mx-1 animate-rotate-y animate-once animate-duration-300"
                 />
-                <div className="place-items-center w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
-                  {theme ? (
-                    <Moon
-                      size={16}
-                      className="h-full mx-1 animate-rotate-y animate-once animate-duration-300"
-                    />
-                  ) : (
-                    <Sun
-                      size={16}
-                      className="h-full mx-1 float-right animate-rotate-y animate-once animate-duration-300"
-                    />
-                  )}
-                </div>
-              </label>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
+              ) : (
+                <Sun
+                  size={16}
+                  className="h-full mx-1 float-right animate-rotate-y animate-once animate-duration-300"
+                />
+              )}
+            </div>
+          </label>
+        </NavbarItem>
+      </NavbarMenu>
+    </Navbar>
   );
 }
--- src/hooks/ToggleMenuHook.tsx
+++ src/hooks/ToggleMenuHook.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function useMenu() {
-  const [toogleMenu, setToogleMenu] = useState<boolean>(false);
+  const [toogleMenu, setToogleMenu] = useState(false);
 
   const handleClick = () => {
     setToogleMenu(!toogleMenu);
